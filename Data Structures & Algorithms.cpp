b3ff97379402273ceeb8de9e5010e52c31292591
Abdulrahman Nabil <abdulrahman.nabil.alqemam@gmail.com>
2025-11-04 20:52:16 +0200

    Pointers & Reference

--- Data Structures & Algorithms.cpp
+++ Data Structures & Algorithms.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 struct Rectangle
@@ -30,11 +31,42 @@ int main()
     #pragma endregion
 
     #pragma region Structures
-    Rectangle r1 = { 5,5 };
-    cout << "Area of the rectangle (r1: Length = " << r1.length << " Height = " << r1.heigth << ") = ";
-    cout << r1.heigth * r1.length << endl;
-    cout << "Size of r1 is " << sizeof(r1) << "bytes" << endl;
+    //Rectangle r1 = { 5,5 };
+    //cout << "Area of the rectangle (r1: Length = " << r1.length << " Height = " << r1.heigth << ") = ";
+    //cout << r1.heigth * r1.length << endl;
+    //cout << "Size of r1 is " << sizeof(r1) << "bytes" << endl;
 
+    #pragma endregion
+
+    #pragma region Pointers & Reference
+    int* arr = (int*)malloc(5 * sizeof(int));
+    int* arr2 = new int[5];
+    Rectangle r = { 5,4 };
+    int five = 5;
+    int *fiveP;
+    Rectangle* rP = new Rectangle{ 4,5 };
+    Rectangle* rP2 = (Rectangle*)malloc(sizeof(Rectangle));
+    rP2->length = 9;
+    rP2->heigth = 11;
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    fiveP = &five;
+    int &fiveR = five;
+    int fiveFive = 55;
+    fiveR = fiveFive;
+    cout << *fiveP << endl;
+    cout << arr << endl;
+    cout << five << endl;
+    cout << rP->length << endl;
+    cout << rP2->heigth << endl;
+
+    free(arr);
+    free(rP2);
+    delete[] arr2;
+    delete rP;
     #pragma endregion
 
     return 0;
