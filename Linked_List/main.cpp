cc5def7cd5f3a5b937847ba5ba77b1b8d68bf32c
Abdulrahman Nabil <abdulrahman.nabil.alqemam@gmail.com>
2025-12-07 20:56:26 +0200

    Linkedlist

---
+++ Linked_List/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+template<class T>
+struct Node {
+    T data;
+    Node* next = nullptr;
+};
+
+template<class T>
+class LinkedList {
+private:
+    Node<T>* head = nullptr;
+
+public:
+    ~LinkedList() {
+        Node<T>* curr = head;
+        while (curr) {
+            Node<T>* nextNode = curr->next;
+            delete curr;
+            curr = nextNode;
+        }
+    }
+
+    void add_items(T* items, int size) {
+        for (int i = 0; i < size; i++) {
+            Node<T>* newNode = new Node<T>{ items[i], nullptr };
+
+            if (!head) {
+                head = newNode;
+            }
+            else {
+                Node<T>* curr = head;
+                while (curr->next) curr = curr->next;
+                curr->next = newNode;
+            }
+        }
+    }
+
+    void display() {
+        Node<T>* curr = head;
+        int i = 1;
+        while (curr) {
+            std::cout << "Node #" << i++ << ": " << curr->data << std::endl;
+            curr = curr->next;
+        }
+    }
+
+    int count() {
+        if (!head) return 0;
+        Node<T>* curr = head;
+        int counter = 0;
+        while (curr != nullptr) {
+            counter++;
+            curr = curr->next;
+        }
+        return counter;
+    }
+    T sum() {
+        Node<T>* curr = head;
+        T total = T();
+        if (!head) return total;
+        while (curr != nullptr) {
+            total += curr->data;
+            curr = curr->next;
+        }
+
+        return total;
+    }
+    T max() {
+        Node<T>* curr = head;
+        if (!head) return T();
+        T max = head->data;
+        while (curr != nullptr) {
+            max = curr->data > max ? curr->data : max;
+            curr = curr->next;
+        }
+        return max;
+    }
+};
+
+
+void main() {
+    int* items = new int[5];
+    items[0] = 75;
+    items[1] = 22;
+    items[2] = 33;
+    items[3] = 44;
+    items[4] = 55;    
+    LinkedList<int>* linkedList = new LinkedList<int>;
+    //head->data = 5;
+    //head->next = new Node<int>;
+    //head->next->data = 6;
+    linkedList->add_items(items, 5);
+    linkedList->display();
+    cout << "counte = " << linkedList->count() << endl;
+    cout << "sum = " << linkedList->sum() << endl;
+    cout << "max = " << linkedList->max() << endl;
+    delete linkedList;
+    return;
+}
