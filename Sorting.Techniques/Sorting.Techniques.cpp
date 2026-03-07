590868f553ba02261d2b240281fd129b81ff1fda
Abdulrahman Nabil <a99nabil@gmail.com>
2026-03-07 14:21:37 +0200

    Quick Sort

--- Sorting.Techniques/Sorting.Techniques.cpp
+++ Sorting.Techniques/Sorting.Techniques.cpp
@@ -54,6 +54,38 @@ void selection_sort(int* array, size_t size)
     }
 }
 
+void quick_sort_internal(int* array, int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int pivot = array[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            std::swap(array[i], array[j]);
+        }
+    }
+
+    std::swap(array[i + 1], array[right]);
+    int pivotIndex = i + 1;
+
+    quick_sort_internal(array, left, pivotIndex - 1);
+    quick_sort_internal(array, pivotIndex + 1, right);
+}
+
+void quick_sort(int* array, size_t size)
+{
+    if (array == nullptr || size < 2)
+        return;
+
+    quick_sort_internal(array, 0, static_cast<int>(size - 1));
+}
+
 void print_array(int* arr, size_t size)
 {
     std::cout << "[";
@@ -82,14 +114,15 @@ void fill_with_random(int* arr, size_t size, int min, int max)
 
 int main()
 {
-    size_t size = 202;
+    size_t size = 11;
     int* nums = new int[size];
 
     fill_with_random(nums, size, 0, 100);
     print_array(nums, size);
     //bubble_sort(nums, size);
     //insertion_sort(nums, size);    
-    selection_sort(nums, size);    
+    //selection_sort(nums, size);    
+    quick_sort(nums, size);    
     print_array(nums, size);
 
     delete[] nums;
